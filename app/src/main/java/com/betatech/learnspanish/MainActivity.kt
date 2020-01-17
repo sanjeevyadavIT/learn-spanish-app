e6574d89b67acd0badf59c9661388835977727f8
Sanjeev Yadav <sanjeevy133@protonmail.com>
2020-01-17 15:24:17 +0530

    feat: Attach Toolbar with Navigational Component

--- app/src/main/java/com/betatech/learnspanish/MainActivity.kt
+++ app/src/main/java/com/betatech/learnspanish/MainActivity.kt
@@ -1,12 +1,26 @@
 package com.betatech.learnspanish
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupToolbarWithNavigationComponent()
+    }
+
+    private fun setupToolbarWithNavigationComponent() {
+        val navController = findNavController(R.id.nav_host_fragment)
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        findViewById<Toolbar>(R.id.toolbar).setupWithNavController(
+            navController,
+            appBarConfiguration
+        )
     }
 }
